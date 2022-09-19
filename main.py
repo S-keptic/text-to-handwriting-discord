7b7752f024ca8872a3f33936a1779fbfd17510ab
sekire <69573771+leviackermannn@users.noreply.github.com>
2022-09-19 16:11:20 +0530

    .

---
+++ main.py
@@ -0,0 +1,41 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+import pywhatkit as pw
+import asyncio
+bot= commands.Bot(command_prefix='?', intents=discord.Intents.all())
+import requests
+
+
+@bot.event
+async def on_ready():
+    print("bot is ready")
+    try: 
+        synced= await bot.tree.sync()
+        print(f"synced {len(synced)} nodes")
+    except Exception as e:
+        print(f"error{e}")
+
+
+
+
+
+
+@bot.tree.command(name="text_to_handwriting" , description="Enter text to convert it into handwritten")
+async def pdff(interaction: discord.Interaction , enter:str):
+    await interaction.response.send_message("`your output will be given shortly`")
+    pw.text_to_handwriting(enter,'demo.png',(0,0,138))
+    
+    channel = bot.get_channel(1017445756275261513)
+    await channel.send(file=discord.File('demo.png'))
+
+    
+   
+
+
+
+
+
+
+bot.run("MTAxODE5NTEzNzA4NTU4NzQ4Ng.G6Tx3X.XX5ll1phw5E9qeejMBBFZJxu22_IdRDpo3xP0U")
+
